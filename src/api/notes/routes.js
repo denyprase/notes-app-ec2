88b8193f34652eb9a250811994beb37ceaadf42e
deny <deny.prasetya.d@mail.ugm.ac.id>
2021-07-04 12:04:28 +0700

    change existing routes to routes of notes plugin

--- src/api/notes/routes.js
+++ src/api/notes/routes.js
@@ -6,31 +6,31 @@ const {
   deleteNoteByIdHandler,
 } = require('./handler');
 
-const routes = [
+const routes = (handler) => [
   {
     method: 'POST',
     path: '/notes',
-    handler: addNoteHandler,
+    handler: handler.postNoteHandler,
   },
   {
     method: 'GET',
     path: '/notes',
-    handler: getAllNoteHandler,
+    handler: handler.getNotesHandler,
   },
   {
     method: 'GET',
     path: '/notes/{id}',
-    handler: getNoteByIdHandler,
+    handler: handler.getNoteByIdHandler,
   },
   {
     method: 'PUT',
     path: '/notes/{id}',
-    handler: editNoteByIdHandler,
+    handler: handler.putNoteByIdHandler,
   },
   {
     method: 'DELETE',
     path: '/notes/{id}',
-    handler: deleteNoteByIdHandler,
+    handler: handler.deleteNoteByIdHandler,
   },
 ];
 
